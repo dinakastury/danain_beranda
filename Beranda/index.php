2fbd17581eeafdd265684937f3663cd685a7a8fe
dinakastury <dinakastury@gmail.com>
2020-11-01 22:06:40 +0700

    commit

---
+++ Beranda/index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Beranda</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="shortcut icon" href="https://kasbon.danain.co.id/assets/img/logo2.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+   </head>
+
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">  
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+      <div class="container">
+        <div class="row align-items-center position-relative">      
+            <div class="site-logo">
+              <a class="navbar-brand img-responsive"><img src="https://kasbon.danain.co.id/assets/img/logo2.png" style="max-width:100px; width: 100px; margin-top: 0px;"></a>
+            </div>    
+                <nav class="site-navigation text-center ml-auto" role="navigation">
+                  <div class="dropdown show">
+                    <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                      <li><a href="#" class="nav-link">Beranda</a></li>
+                      <li><a href="#" class="nav-link">Pendanaan</a></li>
+                      <li><a href="#" class="nav-link">Tentang Kami</a></li>
+                      <li><a href="#" class="nav-link">Cara Kerja</a></li>
+                      <li><button class="dropdown-toggle" href="#" class="nav-link">Registrasi</button></li>
+                      <li><button href="#" class="nav-link">Masuk</button></li>
+                  </div>
+                </ul>
+              </div>
+            </nav>
+        </div>
+      </div>
+    </header>
+    
+    <div class="owl-carousel slide-one-item">
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('img/1.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+          </div>
+        </div>
+      </div>
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('img/2.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section" id="about-section">
+        <h3>Mulai Pendanaan dengan 3 langkah mudah</h3>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+                <img src="img/3.jpg" alt="Image" class="img-fluid shadow p-3 mb-5 bg-white rounded">
+          </div>
+          <div class="col-sm">
+                <img src="img/4.jpg" alt="Image" class="img-fluid shadow p-3 mb-5 bg-white rounded">
+          </div>
+          <div class="col-sm">
+                <img src="img/6.jpg" alt="Image" class="img-fluid shadow p-3 mb-5 bg-white rounded">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section-cover">
+      <img src="img/5.jpg">
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">   
+            <h2>Kenapa harus danain?</h2>
+              <p>Pendanaan yang Anda salurkan melalui platform Danain selalu beragunan jadi lebih aman. Agunan tersebut disimpan dengan standard keamanan terbaik oleh mitra pergadaian kami dan di asuransikan di perusahaan Asuransi terpercaya. Dengan begitu anda bisa tidur nyenyak, sambil menunggu uang Anda berkembang di DANAIN.
+              #YangPastiPastiAja</p>
+          </div>
+          <div class="col-sm">
+            <figure class="block-img-video-1" data-aos="fade">
+              <a href="https://www.youtube.com/watch?v=iURgcp0SyRQ" class="popup-vimeo">
+                <span class="icon"><span class="icon-play"></span></span>
+                <img src="img/1.jpg" alt="Image" class="img-fluid shadow p-3 mb-5 bg-white rounded">
+              </a>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="site-footer" style="background-color: #006600">
+      <div class="container">
+        <div class="row">
+                <div class="col-sm">
+                <h2 class="footer-heading mb-4" style="color: #fff">Legal</h2>
+                  <ul>
+                    <li>Syarat dan Ketentuan</li>
+                    <li>Kebijakan Privasi</li>
+                </ul>
+              </div>
+              <div class="col-sm">
+                <h2 class="footer-heading mb-4" style="color: #fff">Links</h2>
+                  <ul>
+                    <li>Informasi</li>
+                    <li>Bantuan</li>
+                    <li>Pengaduan Pengguna</li>
+                    <li>Karier</li>
+                    <li>Blog & News</li>
+                </ul>
+          </div>
+          <div class="col-sm">
+              <h2 class="footer-heading mb-4" style="color: #fff">Media</h2>
+                <ul>
+                  <li>Media</li>
+                  <li>Instagram</li>
+                  <li>Twitter</li>
+                  <li>Facebook</li>
+                  <li>LinkedIn</li>
+                  <li>danain</li>
+                </ul>             
+            </form>
+          </div>
+        </div>
+        <!-- <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p style="color:#fff">
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> DANAIN
+            </p>
+            </div>
+          </div>
+        </div>
+      </div> -->
+    </footer>
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/main.js"></script>    
+  </body>
+</html>
